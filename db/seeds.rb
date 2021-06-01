57aed8a3dc6624e57b19608e1f6267a1d7268a88
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-01 20:22:05 -0300

    Users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 users = User.create(
   [ 
-    {name: "Admin", email: "admin@email.com", password: "12345678rB"},
-    {name: "Suport", email: "suport@email.com", password: "123456789rB"}
+    {name: "Admin", email: "admin@email.com", password: "12345678rB", password_confirmation: "12345678rB", profile: 0},
+    {name: "Suport", email: "suport@email.com", password: "123456789rB", password_confirmation: "123456789rB", profile: 0}
   ]
 )
