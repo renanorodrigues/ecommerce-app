2ec68a31d89572a6ec64e3525866ea523da5c576
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-02 07:40:39 -0300

    Configurations for translates pt-BR

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
