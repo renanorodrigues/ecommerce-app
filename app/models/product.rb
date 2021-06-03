0e9aaf478273d77c8d86794d5a67ead2fc887338
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-03 11:44:59 -0300

    Associations has_many with ProductCategory test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+
   validates :description, presence: :true
   validates :name, presence: :true, uniqueness: { case_sensitive: false }
   validates :price, presence: :true, numericality: { greater_than: 0 }
