e4af738a402952612f62fdb7bc90b62b8a97f5fe
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-03 10:19:17 -0300

    Association has_many with games

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -1,4 +1,5 @@
 class SystemRequirement < ApplicationRecord
+  has_many :games, dependent: :restrict_with_error
   validates :name, :storage, :processor, :memory, :video_board, :operational_system, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 end
