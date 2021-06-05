92c6d7da0347e8d1458b45d2bf67e62b2015d3b5
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-04 21:50:23 -0300

    Better tests applied

--- spec/models/product_category_spec.rb
+++ spec/models/product_category_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ProductCategory, type: :model do
-  it { is_expected.to belong_to :category }
-  it { is_expected.to belong_to :product }
+  describe 'associations' do
+    it { is_expected.to belong_to :category }
+    it { is_expected.to belong_to :product }
+  end
 end
