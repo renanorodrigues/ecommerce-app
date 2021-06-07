25118956e852a7c9598def09ed644144b9510abd
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-07 20:45:56 -0300

    Add custom validation in due_data

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,6 @@
 class Coupon < ApplicationRecord
-  validates :name, :code, :due_data, :status, :discount_value, :max_use, presence: true
+  validates :name, :code, :status, :discount_value, :max_use, presence: true
+  validates :due_data, presence: true, future_date: true
   validates :code, uniqueness: { case_sensitive: false }
   validates :discount_value, numericality: { greater_than: 0 }
   validates :max_use, numericality: { only_integer: true, greater_than_or_equal_to: 0}
