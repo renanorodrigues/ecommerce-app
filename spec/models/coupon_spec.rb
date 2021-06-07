439c22844385a4ac61259e056a888758795a2420
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-07 20:46:17 -0300

    Add custom validation

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -5,12 +5,30 @@ RSpec.describe Coupon, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:code) }
     it { is_expected.to validate_uniqueness_of(:code).case_insensitive }
-    it { is_expected.to validate_presence_of(:due_data) }
     it { is_expected.to validate_presence_of(:status) }
     it { is_expected.to validate_presence_of(:max_use) }
     it { is_expected.to validate_numericality_of(:max_use).only_integer.is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_presence_of(:discount_value) }
     it { is_expected.to validate_numericality_of(:discount_value).is_greater_than(0) }
     it { is_expected.to define_enum_for(:status).with_values(active: 0, inactive: 1) }
+    it { is_expected.to validate_presence_of(:due_data) }
+
+    it "cant't have past due_data" do
+      subject.due_data = 1.day.ago
+      subject.valid?
+      expect(subject.errors.keys).to include :due_data
+    end
+
+    it 'is invalid with current due_data' do
+      subject.due_data = Time.zone.now
+      subject.valid?
+      expect(subject.errors.keys).to include :due_data
+    end
+
+    it 'is valid with future date' do
+      subject.due_data = Time.zone.now + 1.hour
+      subject.valid?
+      expect(subject.errors.keys).to_not include :due_data
+    end
   end
 end
