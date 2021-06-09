b37fc4498b519e3439e2bcecab8fb4d7481e3d18
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-09 19:32:18 -0300

    correction faker syntaxe

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { Fakker::Name.first_name }
-    profile { %i(admin client).sample }
+    name { Faker::Name.first_name }
+    profile { :admin }
     email { Faker::Internet.email }
     password { "123456" }
     password_confirmation { "123456" }
