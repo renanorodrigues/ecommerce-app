cde527fecb9995fff10ef612e5e4d8f7a48b240c
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-06-10 20:58:50 -0300

    add method create

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -3,5 +3,24 @@ module Admin::V1
     def index
       @categories = Category.all
     end
+
+    def create
+      @category = Category.new(category_params)
+      category_save!
+    end
+
+    private
+
+    def category_params
+      return {} unless params.has_key?(:category)
+      params.require(:category).permit(:id, :name)
+    end
+
+    def category_save!
+      @category.save!
+      render :show
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
   end
 end
