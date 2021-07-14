02c8b30690c6f4edab90fe7689639d840fdb3b44
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-07-14 20:19:17 -0300

    migration add feature to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_233729) do
+ActiveRecord::Schema.define(version: 2021_07_14_224547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2021_06_28_233729) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
+    t.boolean "featured", default: false
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable"
   end
 
